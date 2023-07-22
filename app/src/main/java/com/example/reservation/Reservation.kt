ec6491b92fa6e72b48990037080f0043d677a54c
DESKTOP-QVD6AHH\rotqj <rotqjfrkffo@gmail.com>
2023-07-22 20:22:08 +0900

    first push

--- app/src/main/java/com/example/reservation/Reservation.kt
+++ app/src/main/java/com/example/reservation/Reservation.kt
@@ -20,18 +20,18 @@ class Reservation {
         }
     }
 
-    fun inputVal(x: String): Any? {
-        return when (x) {
+    fun inputVal(value1: String): Any? {
+        return when (value1) {
             "num" -> {
                 while (true) {
                     try {
                         println("1.방예약  2.예약목록출력  3.예약목록(정렬)출력  4.시스템 종료  5.금액(입금-출금)내역목록출력  6.예약 변경/취소")
-                        var y = readLine()!!.toInt()
-                        if (y?.toInt() == 4) {
+                        var value2 = readLine()!!.toInt()
+                        if (value2?.toInt() == 4) {
                             println("호텔 예약 시스템을 종료합니다.")
                             return System.exit(0)
-                        } else if (y != 4 && y < 7) {
-                            return y
+                        } else if (value2 != 4 && value2 < 7) {
+                            return value2
                         } else {
                             println("올바르지 않은 입력입니다. 선택영역은 1~6 이내입니다.")
                         }
@@ -42,16 +42,16 @@ class Reservation {
             }
             "name" -> {
                 println("예약자분의 성함을 입력해주세요.")
-                var y: String? = readLine()
-                return y
+                var value2: String? = readLine()
+                return value2
             }
             "roomNumber" -> {
                 while (true) {
                     try {
                         println("예약할 방 번호를 입력해주세요.")
-                        var y = readLine()!!.toInt()
-                        if (y > 99 && y < 1000) {
-                            return y
+                        var value2 = readLine()!!.toInt()
+                        if (value2 > 99 && value2 < 1000) {
+                            return value2
                         } else {
                             println("올바르지 않은 방 번호 입니다. 방번호는 100 ~ 999 이내 영역입니다.")
                         }
@@ -61,21 +61,19 @@ class Reservation {
                 }
             }
             "checkIn" -> {
-//                체크인 날짜는 지금 날짜와 비교해서 이전날짜는 입력받을 수 없고
-//                체크아웃 날짜는 체크인 날짜보다 이전이거나 같을 수는 없어요
-                val current = LocalDateTime.now()
-                val formatter = DateTimeFormatter.ofPattern("yyMMdd")
-                val formatted = current.format(formatter)
+                var current = LocalDateTime.now()
+                var formatter = DateTimeFormatter.ofPattern("yyMMdd")
+                var formatted = current.format(formatter)
                 while(true){
                     println("체크인 날짜를 입력해주세요. 표기형식: ${formatted} ")
                     try{
-                        var y = readLine().toString()
-                        var setValue : Boolean = ValidDate().isDate(y)
-                        if(setValue == true){
-                            return y
+                        var value2 = readLine().toString()
+                        var setValue = ValidDate().isDate(value2)
+                        if(formatted.toInt() <= setValue && setValue != 0){
+                            println("호텔 예약이 완료되었습니다.")
+                            return setValue
                         }else{
-                            // 수정 필요
-                            println("올바르지 않은 표기형식 입니다. 표기형식에 맞춰 다시 입력하세요. ")
+                            println("이전날짜 또는 존재하지 않는 날짜를 입력하셨습니다. 표기형식에 맞춰 다시 입력하세요. ")
                         }
                     }catch (e: Exception){
                         println("올바르지 않은 표기형식 입니다. 표기형식에 맞춰 다시 입력하세요. ")
--- app/src/main/java/com/example/reservation/ValidDate.kt
+++ app/src/main/java/com/example/reservation/ValidDate.kt
@@ -5,18 +5,15 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-class ValidDate(){
-    fun isDate(Date : String ) : Boolean{
-        val dateFormat = DateTimeFormatter.ofPattern("yyMMdd")
-        try{
-            val current = LocalDateTime.now()
-            val formatter = DateTimeFormatter.ofPattern("yyMMdd")
-            //val dateFormat = current.format(formatter)
-            val date = LocalDate.parse(Date, dateFormat)
-            println("찍힌 값 ${date}")
-            return true
-        }catch(e: Exception){
-            return false
+class ValidDate() {
+    fun isDate(Date: String): Int {
+        var dateFormat = DateTimeFormatter.ofPattern("yyMMdd")
+        try {
+            var date = LocalDate.parse(Date, dateFormat).toString()
+            var temp = date.split("-")
+            return (temp[0].substring(2, 4) + temp[1] + temp[2]).toInt()
+        } catch (e: Exception) {
+            return 0
         }
     }
 
